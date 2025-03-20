db9ba00c1c9d6ce92fc9a59642ada736c5228647
Carlos Galindo <utm22030587@utma.edu.mx>
2025-03-20 16:17:23 -0600

    feat: set up swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Yo Voy Ags App API')
+    .setDescription('The Yo Voy Ags App API description')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
