71d225efcad8e1db408cea9880129278ac123358
Carlos Galindo <utm22030587@utma.edu.mx>
2025-03-20 17:51:23 -0600

    feat: add transactions module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ReportsModule } from './reports/reports.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [ReportsModule],
+  imports: [ReportsModule, TransactionsModule],
 })
 export class AppModule {}
