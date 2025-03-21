6ceffa0aac69cdbedf8cf6d5585c6a61b83dfe1b
Carlos Galindo <utm22030587@utma.edu.mx>
2025-03-21 16:09:50 -0600

    feat: add transfer module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ReportsModule } from './reports/reports.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { TransfersModule } from './transfers/transfers.module';
 
 @Module({
-  imports: [ReportsModule, TransactionsModule],
+  imports: [ReportsModule, TransactionsModule, TransfersModule],
 })
 export class AppModule {}
